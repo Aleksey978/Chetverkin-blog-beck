1679fcea1e472f2256a7132abce8ee79e8271f0d
Aleksey978 <70477391+Aleksey978@users.noreply.github.com>
2023-07-07 02:07:29 +0900

    first commit1

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ import { UserController, PostController } from "./controllers/index.js";
 import handleValidationErrors from "./utils/handleValidationErrors.js";
 
 mongoose
-  .connect(
-    "mongodb+srv://alekseyarg4:oLN6Vu4hd0ZMAEb7@cluster0.mtqazr0.mongodb.net/blog?retryWrites=true&w=majority",
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("DB Ok");
   })
@@ -79,7 +77,7 @@ app.patch(
   PostController.update,
 );
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     console.log(err);
   }
